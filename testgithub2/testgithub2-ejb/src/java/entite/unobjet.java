adc0e5f011ffe9373a1229b092b66cde741ce56b
moi <moi@portable>
2014-01-20 14:49:19 +0100

    suppression de commentaire

--- testgithub2/testgithub2-ejb/src/java/entite/unobjet.java
+++ testgithub2/testgithub2-ejb/src/java/entite/unobjet.java
@@ -40,7 +40,8 @@ public class unobjet implements Serializable {
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
+        
+        
         if (!(object instanceof unobjet)) {
             return false;
         }
